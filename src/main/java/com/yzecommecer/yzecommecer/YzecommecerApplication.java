3527b199a7a71b4d3a3bf65a2848d24dd1a597f8
Ualas Henrique <itsfake087@hotmail.com>
2023-08-22 22:56:30 -0300

    Project Created

---
+++ src/main/java/com/yzecommecer/yzecommecer/YzecommecerApplication.java
@@ -0,0 +1,13 @@
+package com.yzecommecer.yzecommecer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzecommecerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YzecommecerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yzecommecer/yzecommecer/YzecommecerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzecommecer.yzecommecer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzecommecerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
