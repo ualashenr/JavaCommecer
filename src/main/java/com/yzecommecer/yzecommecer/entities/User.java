f56134ad8c740f4296596eea7b7bd0ee90d7b749
Ualas Henrique <itsfake087@hotmail.com>
2023-08-30 10:27:27 -0300

    Entity Role created

--- src/main/java/com/yzecommecer/yzecommecer/entities/User.java
+++ src/main/java/com/yzecommecer/yzecommecer/entities/User.java
@@ -2,14 +2,19 @@ package com.yzecommecer.yzecommecer.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -29,6 +34,12 @@ public class User {
 	
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
+	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
 	}
@@ -92,6 +103,23 @@ public class User {
 
 	public List<Order> getOrders() {
 		return orders;
+	}
+	
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
+	
+	public Set<Role> getRoles(){
+		return roles;
+	}
+	
+	public boolean hasRole(String roleName) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
