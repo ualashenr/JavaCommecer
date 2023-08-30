16648e80eb394541a73f000ed168f17503c58bf4
Ualas Henrique <itsfake087@hotmail.com>
2023-08-30 10:04:35 -0300

    Find all

--- src/main/java/com/yzecommecer/yzecommecer/controllers/ProductController.java
+++ src/main/java/com/yzecommecer/yzecommecer/controllers/ProductController.java
@@ -1,9 +1,14 @@
 package com.yzecommecer.yzecommecer.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yzecommecer.yzecommecer.dto.ProductDTO;
 import com.yzecommecer.yzecommecer.services.ProductService;
 
 @RestController
@@ -12,5 +17,10 @@ public class ProductController {
 	
 	@Autowired
 	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+		return ResponseEntity.ok(service.findAll(pageable));
+	}
 	
 }
--- src/main/java/com/yzecommecer/yzecommecer/services/ProductService.java
+++ src/main/java/com/yzecommecer/yzecommecer/services/ProductService.java
@@ -1,8 +1,13 @@
 package com.yzecommecer.yzecommecer.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.yzecommecer.yzecommecer.dto.ProductDTO;
+import com.yzecommecer.yzecommecer.entities.Product;
 import com.yzecommecer.yzecommecer.repositories.ProductRepository;
 
 @Service
@@ -10,5 +15,10 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-		
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable){
+		Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+	}
 }
